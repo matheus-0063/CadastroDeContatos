02b398de5225698a424b9294840d4fa0974b8306
matheus-0063 <matheus.fer.0063@gmail.com>
2022-12-12 21:56:58 -0300

    Primeiras alteracoes no Front

--- CadastroDeContatos/Controllers/HomeController.cs
+++ CadastroDeContatos/Controllers/HomeController.cs
@@ -6,14 +6,7 @@ namespace CadastroDeContatos.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
+       public IActionResult Index()
         {
             return View();
         }
