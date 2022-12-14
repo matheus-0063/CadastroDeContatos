58d93885a4d512a42f7e65ea815498cb76d6685f
matheus-0063 <matheus.fer.0063@gmail.com>
2022-12-13 23:22:13 -0300

    Jquery datatable Implementado e Usuario Model Criado

---
+++ CadastroDeContatos/Enums/PerfilEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace CadastroDeContatos.Enums
+{
+    public enum PerfilEnum
+    {
+        Admin = 1,
+        Padrao = 2
+    }
+}
---
+++ CadastroDeContatos/Models/UsuarioModel.cs
@@ -0,0 +1,17 @@
+﻿using CadastroDeContatos.Enums;
+using System.Reflection.PortableExecutable;
+
+namespace CadastroDeContatos.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public PerfilEnum Perfil { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime? DataAtualizacao { get; set; }
+    }
+}
